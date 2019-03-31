67b5131a4c9594a18ecceded2b36e136e6f70825
wangchunzi <pankeng1988w@aliyun.com>
2019-03-31 13:19:01 +0800

    1

--- src/main/java/websocket/GetHttpSessionConfigurator.java
+++ src/main/java/websocket/GetHttpSessionConfigurator.java
@@ -1,18 +1,18 @@
-package websocket;
-
-import javax.servlet.http.HttpSession;
-import javax.websocket.HandshakeResponse;
-import javax.websocket.server.HandshakeRequest;
-import javax.websocket.server.ServerEndpointConfig;
-import javax.websocket.server.ServerEndpointConfig.Configurator;
-
-public class GetHttpSessionConfigurator extends Configurator {
-
-    @Override
-    public void modifyHandshake(ServerEndpointConfig config, HandshakeRequest request, HandshakeResponse response) {
-        HttpSession httpSession = (HttpSession) request.getHttpSession();
-        if (httpSession != null) {
-            config.getUserProperties().put(HttpSession.class.getName(), httpSession);
-        }
-    }
-}
+//package websocket;
+//
+//import javax.servlet.http.HttpSession;
+//import javax.websocket.HandshakeResponse;
+//import javax.websocket.server.HandshakeRequest;
+//import javax.websocket.server.ServerEndpointConfig;
+//import javax.websocket.server.ServerEndpointConfig.Configurator;
+//
+//public class GetHttpSessionConfigurator extends Configurator {
+//
+//    @Override
+//    public void modifyHandshake(ServerEndpointConfig config, HandshakeRequest request, HandshakeResponse response) {
+//        HttpSession httpSession = (HttpSession) request.getHttpSession();
+//        if (httpSession != null) {
+//            config.getUserProperties().put(HttpSession.class.getName(), httpSession);
+//        }
+//    }
+//}
